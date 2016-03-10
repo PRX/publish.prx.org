892ce9b49ec43c24caf7ed5923557ffd713ea799
cavis <ryancavis@gmail.com>
2016-03-10 10:38:51 -0700

    Init dev boilerplate

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const gulp  = require('gulp');
+const jade  = require('gulp-jade');
+const seq   = require('gulp-sequence');
+const shell = require('gulp-shell');
+
+// Public tasks (serial)
+gulp.task('git:hooks:pre-commit', seq('jspm:unbundle'));
+gulp.task('postinstall',          seq('jspm:install', 'typings:install', 'git:hooks:install'));
+gulp.task('start',                seq('build:dev', 'server:dev'));
+
+// Build tasks (parallel)
+gulp.task('build:dev', ['jade:index:dev', 'jspm:bundle:dev']);
+
+// Server tasks
+gulp.task('server:dev', shell.task(['lite-server --config=config/dev.bs.config.json']));
+
+// JSPM bundle tasks
+gulp.task('jspm:bundle:dev', shell.task('jspm bundle src/main - [src/app/**/*] ./.dev/vendor.js --inject'));
+gulp.task('jspm:install',    shell.task('jspm install'));
+gulp.task('jspm:unbundle',   shell.task('jspm unbundle'));
+
+// Jade compile tasks
+gulp.task('jade:index:dev', () => {
+  return gulp
+    .src('./src/index.jade')
+    .pipe(jade({ locals: { dist: false } }))
+    .pipe(gulp.dest('./src/'));
+});
+
+// Utility tasks
+const loc = ['#!/bin/sh', 'PATH="/usr/local/bin:$PATH"', 'npm run git:hooks:pre-commit'];
+gulp.task('git:hooks:install', shell.task([
+  `printf '${loc.join('\n')}' > ./.git/hooks/pre-commit`,
+  'chmod +x ./.git/hooks/pre-commit'
+]));
+gulp.task('typings:install', shell.task('typings install'));
