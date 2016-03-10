dc20bfa4b9ada4713f51988c99d7001a33bd6e9b
cavis <ryancavis@gmail.com>
2016-03-10 11:12:31 -0700

    Init app src

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'publish-app',
+  template: '<h1>Publish.prx.org app</h1>'
+})
+
+export class AppComponent { }
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import {enableProdMode} from 'angular2/core';
+import {bootstrap} from 'angular2/platform/browser';
+import {ROUTER_PROVIDERS} from 'angular2/router';
+import {HTTP_BINDINGS} from 'angular2/http';
+
+import 'rxjs/Rx';
+
+if (window.location.host !== 'localhost') {
+  enableProdMode();
+}
+
+import {AppComponent} from './app/app.component';
+
+bootstrap(AppComponent);
