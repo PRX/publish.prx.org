6009c1caf5e806d549012d72484a2fe6fa43175b
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-03-17 11:16:14 -0400

    Route to the upload component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,21 @@
-import {Component} from 'angular2/core';
+import { Component } from 'angular2/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+import { UploadComponent } from './upload/upload.component';
 
 @Component({
   selector: 'publish-app',
-  template: '<h1>Publish.prx.org app</h1>'
+  template: '<router-outlet></router-outlet>',
+  providers: [ROUTER_PROVIDERS],
+  directives: [ROUTER_DIRECTIVES]
 })
+
+@RouteConfig([
+  {
+    path: '/upload',
+    name: 'Upload',
+    component: UploadComponent,
+    useAsDefault: true
+  }
+])
 
 export class AppComponent { }
