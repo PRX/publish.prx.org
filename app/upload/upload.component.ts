528b8a735468c25190ff74cb575d2db78091ce30
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-03-27 11:39:15 -0400

    Reformat imports, inject service, add fileUpload directive

--- app/upload/upload.component.ts
+++ app/upload/upload.component.ts
@@ -1,13 +1,20 @@
-import { Component } from 'angular2/core';
-import { UploadFileSelect } from './upload-file-select.directive';
-import { UploadService } from './upload.service';
-import { MimeTypeService } from '../../util/mime-type.service';
-import { HTTP_PROVIDERS } from 'angular2/http';
+import {Component} from 'angular2/core';
+import {UploadFileSelect} from './upload-file-select.directive';
+import {UploadService} from './upload.service';
+import {MimeTypeService} from '../../util/mime-type.service';
+import {HTTP_PROVIDERS} from 'angular2/http';
+import {FileUpload} from './file-upload.component';
 
 @Component({
-  template: '<input type="file" upload-file />',
+  template: `
+    <input type="file" upload-file />
+    <file-upload *ngFor="#upload of uploadService.uploads" [upload]="upload">
+    </file-upload>
+  `,
   providers: [HTTP_PROVIDERS, UploadService, MimeTypeService],
-  directives: [UploadFileSelect]
+  directives: [UploadFileSelect, FileUpload]
 })
 
-export class UploadComponent { }
+export class UploadComponent {
+  constructor(private uploadService:UploadService) {}
+}
