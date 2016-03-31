c930a9a5f2681d4a1eb3c8f876779a9b54b98231
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-03-31 01:30:49 -0400

    Remove http references

--- app/upload/upload.component.ts
+++ app/upload/upload.component.ts
@@ -2,7 +2,6 @@ import {Component} from 'angular2/core';
 import {UploadFileSelect} from './upload-file-select.directive';
 import {UploadService} from './upload.service';
 import {MimeTypeService} from '../../util/mime-type.service';
-import {HTTP_PROVIDERS} from 'angular2/http';
 import {FileUpload} from './file-upload.component';
 
 @Component({
@@ -11,7 +10,7 @@ import {FileUpload} from './file-upload.component';
     <file-upload *ngFor="#upload of uploadService.uploads" [upload]="upload">
     </file-upload>
   `,
-  providers: [HTTP_PROVIDERS, UploadService, MimeTypeService],
+  providers: [UploadService, MimeTypeService],
   directives: [UploadFileSelect, FileUpload]
 })
 
