b564e90d6cb8ee406b65fcdad247e928b03a8864
cavis <ryancavis@gmail.com>
2016-04-01 08:59:22 -0600

    Start on forms

--- app/storyedit/directives/edit.component.ts
+++ app/storyedit/directives/edit.component.ts
@@ -1,27 +1,56 @@
 import {Component, Input} from 'angular2/core';
+import {NgForm} from 'angular2/common';
 import {StoryModel} from '../models/story.model';
 
 @Component({
+  directives: [NgForm],
   selector: 'newstory-edit',
-  styleUrls: [],
+  styleUrls: ['app/storyedit/storyedit.forms.css'],
   template: `
-    <div *ngIf="story && story.isLoaded">
-      <h1>Edit your content</h1>
-      <p>{{story.title}}</p>
+    <form *ngIf="story && story.isLoaded">
+
+      <fieldset>
+        <legend hidden>Basic Info</legend>
+
+        <label for="title" required>Story Title</label>
+        <p>
+          Write a short, Tweetable title like a newspaper headline.
+          Make viewers want to click on your piece from the title alone.
+        </p>
+        <textarea id="title" required [(ngModel)]="story.title" #title="ngForm"></textarea>
+        <p *ngIf="!title.valid" class="error">Title is required</p>
+
+        <label for="teaser" required>Teaser</label>
+        <p>A first impression; think of this as the single-item lead of a piece.</p>
+        <textarea id="teaser" required [(ngModel)]="story.shortDescription"></textarea>
+
+        <hr/>
+
+        <label required>Story Versions</label>
+        <div class="uploads-box">
+        </div>
+
+        <hr/>
+
+        <label required>Tag your Story</label>
+        <p>
+          Adding tags that are relevant to your piece helps people find your
+          work on PRX and can help you be licensed and heard.
+        </p>
+
+      </fieldset>
+
       <br/><br/>
-      <p>Edit</p>
       <br/><br/>
-      <p>Edit</p>
-      <br/><br/>
-      <p>Edit</p>
-      <br/><br/>
-      <p>Edit</p>
-    </div>
+    </form>
   `
 })
 
 export class EditComponent {
 
   @Input() public story: StoryModel;
+
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.story); }
 
 }
