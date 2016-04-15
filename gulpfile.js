b026d459d2efeb628a1a6f8e6cbecdbc2191a2ba
cavis <ryancavis@gmail.com>
2016-04-15 09:33:55 -0600

    Test using bundle main

--- gulpfile.js
+++ gulpfile.js
@@ -21,6 +21,12 @@ gulp.task('test', function (done) {
     singleRun: true
   }, done).start();
 });
+gulp.task('test:dist', ['jspm:bundle:test'], function (done) {
+  new KarmaServer({
+    configFile: __dirname + '/config/karma.dist.js',
+    singleRun: true
+  }, done).start();
+});
 gulp.task('tdd', function (done) {
   new KarmaServer({
     configFile: __dirname + '/config/karma.conf.js'
@@ -40,14 +46,16 @@ const nonbundle = ['- [app/**/*]', '- [config/**/*]', '- [util/**/*]'].join(' ')
 gulp.task('jspm:bundle:dev', function() {
   return gulp.src('config/systemjs.config.js')
     .pipe(newer('.dev/vendor.js'))
-    .pipe(shell(['jspm bundle ./app/main '+nonbundle+' ./.dev/vendor.js --inject']));
+    .pipe(shell(['jspm bundle ./app/main '+nonbundle+' ./.dev/vendor.js --inject --log warn']));
 });
-gulp.task('jspm:bundle:dist', function() {
-  return gulp.src('config/systemjs.config.js')
-    .pipe(shell(['jspm bundle-sfx ./app/main ./.dist/publish.min.js --minify --no-mangle']));
-});
-gulp.task('jspm:install',    shell.task('jspm install'));
-gulp.task('jspm:unbundle',   shell.task('jspm unbundle'));
+gulp.task('jspm:bundle:test', ['jspm:unbundle'], shell.task([
+  'jspm bundle ./app/main ./.dev/main.js --inject --log warn'
+]));
+gulp.task('jspm:bundle:dist', shell.task([
+  'jspm bundle-sfx ./app/main ./.dist/publish.min.js --minify --no-mangle --log warn'
+]));
+gulp.task('jspm:install',    shell.task('jspm install --log warn'));
+gulp.task('jspm:unbundle',   shell.task('jspm unbundle --log warn'));
 
 // Utility tasks
 const loc = ['#!/bin/sh', 'PATH="/usr/local/bin:$PATH"', 'npm run git:hooks:pre-commit'];
