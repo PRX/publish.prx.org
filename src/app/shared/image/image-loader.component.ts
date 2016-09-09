1f6a571ba58d481adb2d5b0bce163246a9e23690
sandikbarr <sandi.barr@prx.org>
2016-09-09 15:03:58 -0500

    follows prx:image doc in ImageLoaderComponent when provided to handle both error and no image case inside the image loader

--- src/app/shared/image/image-loader.component.ts
+++ src/app/shared/image/image-loader.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, ElementRef } from '@angular/core';
+import { HalDoc } from '../cms/haldoc';
 
 @Component({
   selector: 'image-loader',
@@ -11,6 +12,7 @@ import { Component, Input, ElementRef } from '@angular/core';
 export class ImageLoaderComponent {
 
   @Input() public src: string;
+  @Input() public imageDoc: HalDoc;
 
   static PLACEHOLDER_IMAGE: string = '/assets/images/placeholder.png';
   static PLACEHOLDER_ERROR: string = '/assets/images/placeholder_error.png';
@@ -24,5 +26,17 @@ export class ImageLoaderComponent {
   onLoad = () => this.setBackgroundImage(this.src);
 
   onError = () => this.setBackgroundImage(ImageLoaderComponent.PLACEHOLDER_ERROR);
+
+  ngOnChanges(changes:any) {
+    if ((!changes.src || !changes.src.currentValue) &&
+        changes.imageDoc && changes.imageDoc.currentValue && changes.imageDoc.currentValue.has('prx:image')) {
+      this.imageDoc.follow('prx:image').subscribe(
+        img => this.src = img.expand('enclosure'),
+        err => this.src = ImageLoaderComponent.PLACEHOLDER_ERROR
+      )
+    } else if (!changes.src || !changes.src.currentValue) {
+      this.src = ImageLoaderComponent.PLACEHOLDER_IMAGE;
+    }
+  }
 
 }
