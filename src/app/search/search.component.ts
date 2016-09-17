0dfee557e44753d000f0154b4b9fbe9ee56de79d
sandikbarr <sandi.barr@prx.org>
2016-09-16 20:05:36 -0500

    adds paging support

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -13,15 +13,32 @@ export class SearchComponent implements OnInit {
 
   isLoaded = false;
   totalCount: number;
+  pages: number[];
   noStories: boolean;
   auth: HalDoc;
   stories: StoryModel[];
   storyLoaders: boolean[];
 
+  currentPage: number;
+  showNumPages: number = 10;
+  pagesBegin: number;
+  pagesEnd: number;
+
+  searchTerm: string;
+  genres: string[];
+  subGenres: string[];
+
   constructor(private cms: CmsService) {}
 
   ngOnInit() {
+    this.search('', 1);
+  }
+
+  search(term: string, page: number, per: number = 10) {
+    this.searchTerm = term;
+    this.currentPage = page;
     this.isLoaded = false;
+
     this.cms.follow('prx:authorization').subscribe((auth) => {
       this.auth = auth;
       let storiesCount = auth.count('prx:stories') || 0;
@@ -30,11 +47,17 @@ export class SearchComponent implements OnInit {
         this.isLoaded = true;
       } else {
         this.storyLoaders = Array(storiesCount);
-        auth.followItems('prx:stories').subscribe((stories) => {
+
+        let draftStory = new StoryModel(this.auth);
+        let isUnsavedDraft = draftStory.isNew && draftStory.isStored();
+        let showUnsavedDraft = this.currentPage === 1 && isUnsavedDraft;
+
+        let requestPer = showUnsavedDraft ? per - 1 : per;
+
+        auth.followItems('prx:stories', {page: this.currentPage, per: requestPer}).subscribe((stories) => {
+          this.stories = showUnsavedDraft ? [draftStory] : [];
           this.storyLoaders = null;
           let storyDocs = <HalDoc[]> stories;
-          let draftStory = new StoryModel(this.auth);
-          this.stories = draftStory.isNew && draftStory.isStored() ? [draftStory] : [];
           for (let doc of storyDocs) {
             let story = new StoryModel(this.auth, doc, false);
             // TODO: a lot of these stories on the search page will likely be from the same series, not good that it's getting each just for the title
@@ -47,7 +70,11 @@ export class SearchComponent implements OnInit {
               this.stories.push(story);
             }
           }
-          this.totalCount = auth.count('prx:stories');
+          this.totalCount = isUnsavedDraft ? auth.count('prx:stories') + 1 : auth.count('prx:stories');
+          let totalPages = this.totalCount % per ? Math.floor(this.totalCount) / per + 1 : Math.floor(this.totalCount / per);
+          this.pages = Array.apply(null, {length: totalPages}).map((val, i) => i + 1);
+          this.pagesBegin = this.showNumPages * Math.floor((this.currentPage - 1) / this.showNumPages);
+          this.pagesEnd = this.showNumPages * Math.ceil(this.currentPage / this.showNumPages);
           this.isLoaded = true;
         });
       }
