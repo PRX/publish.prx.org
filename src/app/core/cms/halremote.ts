2029ec3070f5824845fe4fd4413f2816713644a3
cavis <ryancavis@gmail.com>
2016-11-30 10:55:01 -0700

    Rework remotes to allow refreshing token then retrying

--- src/app/core/cms/halremote.ts
+++ src/app/core/cms/halremote.ts
@@ -1,4 +1,4 @@
-import { Http, Headers, RequestOptionsArgs } from '@angular/http';
+import { Http, Headers, RequestOptionsArgs, Response } from '@angular/http';
 import { Observable } from 'rxjs';
 import * as TemplateParser from 'url-template';
 import { HalRemoteCache } from './halremote.cache';
@@ -12,7 +12,8 @@ export class HalRemote {
   constructor(
     private http: Http,
     private host: string,
-    private token: string
+    private token?: Observable<string>,
+    private refreshToken?: () => Observable<string>
   ) {}
 
   expand(link: HalLink, params: {} = null): string {
@@ -34,18 +35,7 @@ export class HalRemote {
       if (cachedResponse) {
         return cachedResponse;
       } else {
-        return HalRemoteCache.set(href, this.http.get(href, this.httpOptions())
-          .catch((res) => {
-            return Observable.of(res);
-          })
-          .flatMap((res) => {
-            if (res.status === 200) {
-              return Observable.of(res.json());
-            } else {
-              return Observable.throw(new Error(`Got ${res.status} from GET ${href}`));
-            }
-          })
-        );
+        return HalRemoteCache.set(href, this.httpRequest('get', href));
       }
     } else {
       return Observable.throw(new Error('No link object specified!'));
@@ -56,45 +46,63 @@ export class HalRemote {
     let href = this.expand(link, params);
     let body = data ? JSON.stringify(data) : null;
     HalRemoteCache.del(href);
-    return this.http.put(href, body, this.httpOptions(true)).map((res) => {
-      if (res.status === 204) {
-        return Observable.of(true);
-      } else {
-        return Observable.throw(new Error(`Got ${res.status} from PUT ${href}`));
-      }
-    });
+    return this.httpRequest('put', href, body);
   }
 
   post(link: HalLink, params: {} = null, data: {}): Observable<{}> {
     let href = this.expand(link, params);
     let body = data ? JSON.stringify(data) : null;
-    return this.http.post(href, body, this.httpOptions(true)).map((res) => {
-      return res.json();
-    });
+    HalRemoteCache.del(href);
+    return this.httpRequest('post', href, body);
   }
 
   delete(link: HalLink, params: {} = null): Observable<{}> {
     let href = this.expand(link, params);
     HalRemoteCache.del(href);
-    return this.http.delete(href, this.httpOptions()).map((res) => {
-      if (res.status === 204) {
-        return Observable.of(true);
-      } else {
-        return Observable.throw(new Error(`Got ${res.status} from DELETE ${href}`));
-      }
-    });
+    return this.httpRequest('delete', href);
   }
 
-  private httpOptions(hasContent = false): RequestOptionsArgs {
+  private httpRequest(method: string, href: string, body?: string, allowRetry = true): Observable<Response> {
+    return this.httpOptions(body ? true : false)
+      .flatMap(opts => {
+        if (body) {
+          return this.http[method](href, body, opts);
+        } else {
+          return this.http[method](href, opts);
+        }
+      })
+      .catch(res => Observable.of(res)) // don't throw http errors
+      .flatMap(res => {
+        if (method === 'get' && res.status === 200) {
+          return Observable.of(res.json());
+        } else if (method === 'put' && res.status === 204) {
+          return Observable.of(true);
+        } else if (method === 'post' && res.status === 201) {
+          return Observable.of(res.json());
+        } else if (method === 'delete' && res.status === 204) {
+          return Observable.of(true);
+        } else if (res.status === 401 && allowRetry && this.refreshToken) {
+          return this.refreshToken().flatMap(() => this.httpRequest(method, href, body, false));
+        } else {
+          return Observable.throw(new Error(`Got ${res.status} from ${method.toUpperCase()} ${href}`));
+        }
+      });
+  }
+
+  private httpOptions(hasContent = false): Observable<RequestOptionsArgs> {
     let headers = new Headers();
     headers.append('Accept', 'application/hal+json');
-    if (this.token) {
-      headers.append('Authorization', `Bearer ${this.token}`);
-    }
     if (hasContent) {
       headers.append('Content-Type', 'application/hal+json');
     }
-    return {headers: headers};
+    if (this.token) {
+      return this.token.first().map(tokenString => {
+        headers.append('Authorization', `Bearer ${tokenString}`);
+        return {headers: headers};
+      });
+    } else {
+      return Observable.of({headers: headers});
+    }
   }
 
 }
