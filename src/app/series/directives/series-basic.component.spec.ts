c2e660571c402cf3269282d70b91ea2ab6002cd2
sandikbarr <sandi.barr@prx.org>
2016-12-13 14:02:20 -0600

    improve spec

--- src/app/series/directives/series-basic.component.spec.ts
+++ src/app/series/directives/series-basic.component.spec.ts
@@ -8,14 +8,17 @@ describe('SeriesBasicComponent', () => {
 
   provide(TabService);
 
-  cit('shows the basic series edit fields', (fix, el, comp) => {
+  cit('does not render until the story is loaded', (fix, el, comp) => {
     expect(el).not.toQuery('publish-fancy-field');
+    expect(el).not.toQuery('publish-wysiwyg');
     comp.series = {changed: () => false};
     fix.detectChanges();
 
     expect(el.queryAll(By.css('publish-fancy-field')).length).toEqual(3);
     expect(el).toContainText('name of your series');
     expect(el).toContainText('short description');
+    expect(el).toContainText('cover image');
+    expect(el.queryAll(By.css('publish-wysiwyg')).length).toEqual(1);
     expect(el).toContainText('full description');
   });
 
