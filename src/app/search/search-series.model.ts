4afda10a6bd09ec702cbb2a2632736e6c2371bc0
sandikbarr <sandi.barr@prx.org>
2016-12-27 09:49:08 -0600

    remove genre from search series model

--- src/app/search/search-series.model.ts
+++ src/app/search/search-series.model.ts
@@ -13,8 +13,6 @@ export class SearchSeries {
   constructor(
     public perPage?: number,
     public text?: string,
-    public genre?: string,
-    public subGenre?: string,
     public orderBy = 'updated_at',
     public orderDesc = true
   ) {  }
@@ -22,8 +20,6 @@ export class SearchSeries {
   fromRouteParams(params) {
     this.perPage = params['perPage'] || 10;
     this.text = params['text'];
-    this.genre = params['genre'];
-    this.subGenre = params['subGenre'];
     this.orderBy = params['orderBy'] || 'updated_at';
     this.orderDesc = params['orderDesc'] === 'true' || params['orderDesc'] === undefined;
   }
