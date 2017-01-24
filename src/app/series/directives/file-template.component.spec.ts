d778dc18afa2c20796591d2b77f418381c12ce6a
cavis <ryancavis@gmail.com>
2017-01-23 17:04:10 -0700

    ANd more tests

--- src/app/series/directives/file-template.component.spec.ts
+++ src/app/series/directives/file-template.component.spec.ts
@@ -1,18 +1,45 @@
-import { cit, create } from '../../../testing';
+import { cit, create, stubPipe } from '../../../testing';
+import { AudioFileTemplateModel } from '../../shared';
 import { FileTemplateComponent } from './file-template.component';
 
 describe('FileTemplateComponent', () => {
 
   create(FileTemplateComponent);
 
+  stubPipe('capitalize');
+
   cit('renders undestroyed file templates', (fix, el, comp) => {
     expect(el).not.toQuery('publish-fancy-field');
-    comp.file = {isDestroy: true};
+    comp.file = new AudioFileTemplateModel(null);
+    comp.file.isDestroy = true;
     fix.detectChanges();
     expect(el).not.toQuery('publish-fancy-field');
-    comp.file = {isDestroy: false};
+    comp.file.isDestroy = false;
     fix.detectChanges();
     expect(el).toQuery('publish-fancy-field');
+  });
+
+  cit('only allows removing the last template in a version', (fix, el, comp) => {
+    comp.file = new AudioFileTemplateModel(null);
+    comp.version = {fileTemplates: [{}, comp.file, {}]};
+    fix.detectChanges();
+    expect(el).not.toQuery('.icon-cancel');
+    comp.version = {fileTemplates: [{}, comp.file, {isDestroy: true}]};
+    fix.detectChanges();
+    expect(el).toQuery('.icon-cancel');
+  });
+
+  cit('shows label and length validation errors', (fix, el, comp) => {
+    comp.file = new AudioFileTemplateModel(null);
+    fix.detectChanges();
+    expect(el).toContainText('label is a required field');
+    comp.file.set('label', 'foobar');
+    fix.detectChanges();
+    expect(el).not.toQuery('.error');
+    comp.file.set('lengthMinimum', 100);
+    comp.file.set('lengthMaximum', 50);
+    fix.detectChanges();
+    expect(el).toContainText('must be less than');
   });
 
 });
---
+++ src/app/shared/form/fancy-duration.component.spec.ts
@@ -0,0 +1,48 @@
+import { cit, create, stubPipe } from '../../../testing';
+import { FancyDurationComponent } from './fancy-duration.component';
+
+fdescribe('FancyDurationComponent', () => {
+
+  create(FancyDurationComponent, false);
+
+  stubPipe('padzero');
+
+  cit('parses seconds to HH:MM:SS', (fix, el, comp) => {
+    comp.model = {foobar: 84716, original: {}};
+    comp.name = 'foobar';
+    fix.detectChanges();
+    expect(comp.total).toEqual(84716);
+    expect(comp.hours).toEqual(23);
+    expect(comp.minutes).toEqual(31);
+    expect(comp.seconds).toEqual(56);
+  });
+
+  cit('detects changes for each subfield', (fix, el, comp) => {
+    comp.model = {foobar: 84716, original: {foobar: 84716}};
+    comp.name = 'foobar';
+    fix.detectChanges();
+    expect([comp.hoursChanged, comp.minutesChanged, comp.secondsChanged]).toEqual([false, false, false]);
+
+    comp.model.foobar = 23 * 60 * 60;
+    fix.detectChanges();
+    expect([comp.hoursChanged, comp.minutesChanged, comp.secondsChanged]).toEqual([false, true, true]);
+
+    comp.model.foobar = 31 * 60 + 56;
+    fix.detectChanges();
+    expect([comp.hoursChanged, comp.minutesChanged, comp.secondsChanged]).toEqual([true, false, false]);
+  });
+
+  cit('sets from subfields', (fix, el, comp) => {
+    comp.model = {foobar: 0, original: {}, set: (f, v) => comp.model[f] = v};
+    comp.name = 'foobar';
+    comp.set('hours', 1);
+    expect(comp.model.foobar).toEqual(3600);
+    comp.set('minutes', 61);
+    expect(comp.model.foobar).toEqual(7260);
+    comp.set('seconds', 52);
+    expect(comp.model.foobar).toEqual(7312);
+    comp.set('seconds', -2);
+    expect(comp.model.foobar).toEqual(7260);
+  });
+
+});
