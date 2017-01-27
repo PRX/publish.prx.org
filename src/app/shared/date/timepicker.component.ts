bec581df0c46e4845f8cb19c5382b5604ff436fd
sandikbarr <sandi.barr@prx.org>
2017-01-27 10:38:03 -0600

    generate strings for option values

--- src/app/shared/date/timepicker.component.ts
+++ src/app/shared/date/timepicker.component.ts
@@ -17,56 +17,15 @@ export class TimepickerComponent {
   @Input() changed: boolean;
 
   localTimezone = new Date().toString().match(/(\([A-Za-z\s].*\))/)[1];
-  options: string[] = [
-    '12:00am ' + this.localTimezone,
-    '12:30am ' + this.localTimezone,
-    '1:00am ' + this.localTimezone,
-    '1:30am ' + this.localTimezone,
-    '2:00am ' + this.localTimezone,
-    '2:30am ' + this.localTimezone,
-    '3:00am ' + this.localTimezone,
-    '3:30am ' + this.localTimezone,
-    '4:00am ' + this.localTimezone,
-    '4:30am ' + this.localTimezone,
-    '5:00am ' + this.localTimezone,
-    '5:30am ' + this.localTimezone,
-    '6:00am ' + this.localTimezone,
-    '6:30am ' + this.localTimezone,
-    '7:00am ' + this.localTimezone,
-    '7:30am ' + this.localTimezone,
-    '8:00am ' + this.localTimezone,
-    '8:30am ' + this.localTimezone,
-    '9:00am ' + this.localTimezone,
-    '9:30am ' + this.localTimezone,
-    '10:00am ' + this.localTimezone,
-    '10:30am ' + this.localTimezone,
-    '11:00am ' + this.localTimezone,
-    '11:30am ' + this.localTimezone,
-    '12:00pm ' + this.localTimezone,
-    '12:30pm ' + this.localTimezone,
-    '1:00pm ' + this.localTimezone,
-    '1:30pm ' + this.localTimezone,
-    '2:00pm ' + this.localTimezone,
-    '2:30pm ' + this.localTimezone,
-    '3:00pm ' + this.localTimezone,
-    '3:30pm ' + this.localTimezone,
-    '4:00pm ' + this.localTimezone,
-    '4:30pm ' + this.localTimezone,
-    '5:00pm ' + this.localTimezone,
-    '5:30pm ' + this.localTimezone,
-    '6:00pm ' + this.localTimezone,
-    '6:30pm ' + this.localTimezone,
-    '7:00pm ' + this.localTimezone,
-    '7:30pm ' + this.localTimezone,
-    '8:00pm ' + this.localTimezone,
-    '8:30pm ' + this.localTimezone,
-    '9:00pm ' + this.localTimezone,
-    '9:30pm ' + this.localTimezone,
-    '10:00pm ' + this.localTimezone,
-    '10:30pm ' + this.localTimezone,
-    '11:00pm ' + this.localTimezone,
-    '11:30pm ' + this.localTimezone
-  ];
+  options: string[] = [];
+
+  constructor() {
+    let day = new Date(1970, 0, 1, 0, 0, 0, 0);
+    while (day.getDate() === 1) {
+      this.options.push(this.dateToHumanTime(day));
+      day.setMinutes(day.getMinutes() + 30);
+    }
+  }
 
   roundMinutes(value: number): string {
     return value > 0 && value <= 30 ? '30' : '00';
