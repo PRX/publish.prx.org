63ea300ad880a345f010cc582143d3dfc2df241f
gcampo88 <gigi.campo@gmail.com>
2017-02-14 15:07:02 -0500

    Move series-basic html to own file and update owner section

--- src/app/series/directives/series-basic.component.ts
+++ src/app/series/directives/series-basic.component.ts
@@ -5,44 +5,7 @@ import { SeriesModel, TabService } from '../../shared';
 import { SeriesComponent } from '../series.component';
 
 @Component({
-  template: `
-    <form *ngIf="series">
-      <publish-fancy-field *ngIf="series.isNew && accountOptions?.length > 1" required 
-        label="Owner" name="accountId" [select]="accountOptions" [model]="series" (onChange)="setAccount($event)">
-        <div class="fancy-hint">Select the account under which to create this series.</div>
-      </publish-fancy-field>
-      
-      <publish-fancy-field *ngIf="!series.isNew && accountOptions?.length > 1" label="Owner">
-        <div class="fancy-hint">This series is owned by <strong>{{accountName}}</strong>.</div>
-      </publish-fancy-field>
-    
-      <publish-fancy-field textinput required [model]="series" name="title" label="Series Title" required>
-        <div class="fancy-hint">What's the name of your series?</div>
-      </publish-fancy-field>
-
-      <publish-fancy-field textinput required [model]="series" name="shortDescription" label="Teaser" required>
-        <div class="fancy-hint">A short description of your series.</div>
-      </publish-fancy-field>
-
-      <publish-fancy-field label="Description">
-        <div class="fancy-hint">A full description of your series.</div>
-        <publish-wysiwyg [model]="series" name="description" [content]="series.description"
-          [images]="series.images" [changed]="descriptionChanged"></publish-wysiwyg>
-      </publish-fancy-field>
-      
-      <publish-fancy-field label="Profile Image">
-        <div class="fancy-hint">This image will be used as the cover image for your series.</div>
-        <publish-image-upload [model]="series" purpose="profile" minWidth=1400 minHeight=1400></publish-image-upload>
-      </publish-fancy-field>
-
-      <publish-fancy-field label="Thumbnail Image">
-        <div class="fancy-hint">Optionally provide an alternate image to use for smaller
-          display purposes. If not provided, a rescaled thumbnail of your profile image
-          will be used.</div>
-        <publish-image-upload [model]="series" purpose="thumbnail" suggestSize="300 x 300"></publish-image-upload>
-      </publish-fancy-field>
-    </form>
-  `
+  templateUrl: 'series-basic.component.html'
 })
 
 export class SeriesBasicComponent implements OnDestroy {
