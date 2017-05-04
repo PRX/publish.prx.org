3022ccdb7a65a11aedbb2f900538fa840e49f1b9
cavis <ryancavis@gmail.com>
2017-05-04 14:46:18 -0600

    TS 2.1 breaks error extending

--- src/app/core/audio/playback/playback.ts
+++ src/app/core/audio/playback/playback.ts
@@ -5,7 +5,13 @@ export interface PlaybackMetadata {
   progress: number;
 }
 
+// NOTE: https://github.com/Microsoft/TypeScript-wiki/blob/master/Breaking-Changes.md
+//       #extending-built-ins-like-error-array-and-map-may-no-longer-work
 export class UnsupportedFileError extends Error {
+  constructor(m: string) {
+    super(m);
+    Object.setPrototypeOf(this, UnsupportedFileError.prototype);
+  }
 }
 
 export interface AudioPlayback {
