4892f1207cc0d579a531251ad23fcb6328e8641c
Andrew Kuklewicz <andrew@beginsinwonder.com>
2017-09-13 14:30:37 -0400

    get the enclosure url for the episode, if there is one

--- src/app/shared/model/feeder-episode.model.ts
+++ src/app/shared/model/feeder-episode.model.ts
@@ -12,11 +12,12 @@ export class FeederEpisodeModel extends BaseModel {
 
   // writeable
   SETABLE = ['guid', 'authorName', 'authorEmail', 'episodeUrl', 'summary'];
-  URLS = ['episodeUrl'];
+  URLS = ['episodeUrl', 'enclosureUrl'];
   guid = '';
   authorName = '';
   authorEmail = '';
   episodeUrl = '';
+  enclosureUrl = '';
   summary = '';
 
   VALIDATORS = {
@@ -52,6 +53,9 @@ export class FeederEpisodeModel extends BaseModel {
     this.authorName = author['name'] || '';
     this.authorEmail = author['email'] || '';
     this.summary = this.doc['summary'] || '';
+    if (this.doc['_links'] && this.doc['_links']['enclosure'] && this.doc['_links']['enclosure']['href']) {
+      this.enclosureUrl = this.doc['_links']['enclosure']['href'] || '';
+    }
   }
 
   encode(): {} {
