709f15f5801366915894e6554b1274bcf525dfa4
Peter Karman <pkarman@users.noreply.github.com>
2018-04-16 17:46:22 -0400

    Permission denied route (#507)
    * Add permission-denied route
    
    * Add /permission-denied route
    
    * Avoid string comparison, use AuthService.parseToken instead
    
    * Keep nav bar from "jumping" when authz is denied
    
    * Use the new Userinfo service to populate nav
    
    * Clean up now-unused code
    
    * Bump styleguide version
    
    * Update auth tests to use Userinfo
    
    * navuser component uses userinfo not userName
    
    * Bump styleguide version
    
    * Bump styleguide version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Angulartics2GoogleAnalytics } from 'angulartics2';
-import { AuthService } from 'ngx-prx-styleguide';
+import { AuthService, Userinfo, UserinfoService } from 'ngx-prx-styleguide';
 import { CmsService, HalDoc } from './core';
 import { Env } from './core/core.env';
 
@@ -15,29 +15,44 @@ export class AppComponent {
   authClient = Env.AUTH_CLIENT_ID;
 
   loggedIn = true; // until proven otherwise
+  authorized = false; // until proven otherwise, to avoid nav "jump"
   userName: string;
   userImageDoc: HalDoc;
+  userinfo: Userinfo;
 
   constructor(
     private auth: AuthService,
     private cms: CmsService,
+    private user: UserinfoService,
     private angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics
   ) {
     auth.token.subscribe(token => this.loadAccount(token));
+    this.user.config(this.authHost);
   }
 
   loadAccount(token: string) {
     if (token) {
+      this.user.getUserinfo().subscribe(userinfo => this.loadUserinfo(userinfo));
+
       this.loggedIn = true;
-      this.cms.individualAccount.subscribe(doc => {
-        this.userImageDoc = doc;
-        this.userName = doc['name'];
-      });
+      if (!this.auth.parseToken(token)) {
+        this.authorized = false;
+      } else {
+        this.authorized = true;
+      }
     } else {
       this.loggedIn = false;
+      this.authorized = false;
       this.userImageDoc = null;
       this.userName = null;
     }
+  }
+
+  loadUserinfo(userinfo: Userinfo) {
+    this.userinfo = userinfo;
+    this.user.getUserDoc(userinfo).subscribe(userDoc => {
+      this.userImageDoc = userDoc;
+    });
   }
 
 }
