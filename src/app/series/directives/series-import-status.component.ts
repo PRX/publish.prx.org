6207f06cbce3fb0a8504014559728796606b4169
Sam Vevang <sam.vevang@gmail.com>
2018-10-26 09:49:36 -0500

    fixup! poll for changes in the series component

--- src/app/series/directives/series-import-status.component.ts
+++ src/app/series/directives/series-import-status.component.ts
@@ -4,7 +4,6 @@ import { Observable } from 'rxjs/Observable';
 import { CmsService, HalDoc } from '../../core';
 import { SeriesModel, SeriesImportModel } from '../../shared';
 import { TabService } from 'ngx-prx-styleguide';
-import { SeriesImportService } from '../series-import.service';
 import { map } from 'rxjs/operators';
 
 @Component({
@@ -17,7 +16,6 @@ export class SeriesImportStatusComponent implements OnDestroy {
   // An observable on an array of observables on series import instances
   // Load the series imports, then create a observable stream for each one
   // polling until we navigate away or the import finishes
-  seriesImports: Observable<Observable<SeriesImportModel>[]>;
   tabSub: Subscription;
 
   ngOnInit(){}
