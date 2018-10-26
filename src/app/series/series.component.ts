21492c93a95a63590705c75b2740bd0f6de0fd83
Sam Vevang <sam.vevang@gmail.com>
2018-10-26 09:50:14 -0500

    comment for the polling logic

--- src/app/series/series.component.ts
+++ src/app/series/series.component.ts
@@ -79,6 +79,10 @@ export class SeriesComponent implements OnInit {
 
   buildSeries(parent: any, series: any) {
     let newSeries = new SeriesModel(parent, series) 
+    // Because the series state is split between component and model, make the
+    // loadSeries method idempotent and preserve the podcast import polling 
+    // state with each call. Then refresh the series model based on import 
+    // status polling intervals.
     if(this.series){
       newSeries.seriesImports = this.series.seriesImports;
     }
