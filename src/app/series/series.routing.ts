71fa48a654994bb6eef89ec1838ee48ddc5e0199
cavis <ryancavis@gmail.com>
2019-04-09 10:59:43 -0600

    Plan episodes route

--- src/app/series/series.routing.ts
+++ src/app/series/series.routing.ts
@@ -5,6 +5,7 @@ import { AuthGuard, DeactivateGuard } from 'ngx-prx-styleguide';
 import { SeriesComponent } from './series.component';
 import { SeriesBasicComponent } from './directives/series-basic.component';
 import { SeriesTemplatesComponent } from './directives/series-templates.component';
+import { SeriesPlanComponent } from './directives/series-plan.component';
 import { SeriesPodcastComponent } from './directives/series-podcast.component';
 import { SeriesFeedComponent } from './directives/series-feed.component';
 import { SeriesImportStatusComponent } from './directives/series-import-status.component';
@@ -13,6 +14,7 @@ import { FileTemplateComponent } from './directives/file-template.component';
 const seriesChildRoutes = [
   { path: '',          component: SeriesBasicComponent },
   { path: 'templates', component: SeriesTemplatesComponent },
+  { path: 'plan',      component: SeriesPlanComponent },
   { path: 'podcast',   component: SeriesPodcastComponent },
   { path: 'list',   component: SeriesFeedComponent },
   { path: 'import-status',   component: SeriesImportStatusComponent }
@@ -39,6 +41,7 @@ export const seriesComponents: any[] = [
   SeriesComponent,
   SeriesBasicComponent,
   SeriesTemplatesComponent,
+  SeriesPlanComponent,
   SeriesPodcastComponent,
   SeriesFeedComponent,
   SeriesImportStatusComponent,
